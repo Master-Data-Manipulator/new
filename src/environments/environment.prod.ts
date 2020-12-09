d46a60f89201cf580eee6f57a9247b921bc5f910
Master-Data-Manipulator <65172980+Master-Data-Manipulator@users.noreply.github.com>
2020-12-09 02:55:24 -0600

    first commit
    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAv2mAqBzUqsdYJVyCmeTD2uQr9XnMJRrg",
+    authDomain: "fire-base-fa6cb.firebaseapp.com",
+    projectId: "fire-base-fa6cb",
+    storageBucket: "fire-base-fa6cb.appspot.com",
+    messagingSenderId: "872808220873",
+    appId: "1:872808220873:web:a36fe15104b28ffd8ce518",
+    measurementId: "G-Q86FLP3WQ5"
+  }
+};
