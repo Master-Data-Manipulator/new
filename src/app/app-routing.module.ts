279a8effa7f37e47c6fe12e87e16a2cedbfbc60f
Master-Data-Manipulator <65172980+Master-Data-Manipulator@users.noreply.github.com>
2020-12-15 00:24:37 -0600

    update before database changes made

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,12 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 import { SignInComponent } from '../app/components/sign-in/sign-in.component';
 import { SignUpComponent } from '../app/components/sign-up/sign-up.component';
-import { DashboardComponent } from '../app/components/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from '../app/components/forgot-password/forgot-password.component';
 import { AuthGuard } from "../app/shared/guard/auth.guard";
 import { VerifyEmailComponent } from '../app/components/verify-email/verify-email.component';
 import { TermsComponent } from '../app/components/bottom-bar/terms/terms.component';
 import { ContactComponent } from '../app/components/bottom-bar/contact/contact.component';
-import { CertificateHomeComponent } from './pages/certificate-home/certificate-home.component';
+
 
 
 
@@ -17,7 +16,6 @@ const routes: Routes = [
   { path: '', loadChildren: './pages/menu/menu.module#MenuPageModule' },
   { path: 'sign-in', component: SignInComponent },
   { path: 'register-user', component: SignUpComponent },
-  { path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'verify-email-address', component: VerifyEmailComponent },
   { path: 'terms', component: TermsComponent },
@@ -25,7 +23,6 @@ const routes: Routes = [
   { path: 'developers', loadChildren: './pages/developers/developers.module#DevelopersPageModule' },
   { path: 'developers/:id', loadChildren: './pages/developer/developer.module#DeveloperPageModule' },
   { path: 'menu', loadChildren: './pages/menu/menu.module#MenuPageModule' },
-  { path: 'certificate-home', component: CertificateHomeComponent },
   {
     path: 'menu',
     loadChildren: () => import('./pages/menu/menu.module').then(m => m.MenuPageModule)
